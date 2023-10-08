d8601d4ca85ba44ab4a3089b4df06d6e9ba543b0
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2023-10-08 11:00:08 -0500

    layout grid and data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), svelte(), react()],
+  output: 'server'
+});
