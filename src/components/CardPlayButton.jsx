7b0a4a69fa23837dd57dce96489ac065fd52915d
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2023-10-12 20:47:01 -0500

    songs table

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,8 +1,9 @@
 import { usePlayerStore } from '@/store/playerStore'
-import { Pause } from './Pause'
-import { Play } from './Play'
+import { PauseLarge } from './PauseLarge'
+import { PlayLarge } from './PlayLarge'
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
+  const btnSizeClassName = size === 'small' ? 'w-12 h-12' : 'w-14 h-14'
   const { isPlaying, currentMusic, setIsPlaying, setCurrentMusic } = usePlayerStore(
     (state) => state
   )
@@ -27,10 +28,10 @@ export function CardPlayButton({ id }) {
 
   return (
     <button
-      className='card-play-button rounded-full bg-green-500 shadow-2xl p-4'
+      className={`card-play-button flex justify-center items-center rounded-full bg-green-500 shadow-2xl ${btnSizeClassName} hover:scale-105 transition hover:bg-green-400`}
       onClick={handleClick}
     >
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <PauseLarge /> : <PlayLarge />}
     </button>
   )
 }
