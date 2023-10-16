e0c572f4374b6b97275241befedb3765f37ddf12
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2023-10-16 02:20:49 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
+import react from '@astrojs/react'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
-});
+  output: 'server',
+  adapter: vercel()
+})
